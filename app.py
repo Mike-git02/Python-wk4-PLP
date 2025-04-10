c2ec82f663965e889077c8aaa47caefa5661cd58
Mike-git02 <onyimbomichael02@gmail.com>
2025-04-10 11:54:57 +0300

    wk4 python assignment

---
+++ app.py
@@ -0,0 +1,10 @@
+# Open the file in write mode ("w")
+file = open("example.txt", "w")
+
+# Write "Hello, World!" into the file
+file.write("Hello, World!")
+
+# Close the file
+file.close()
+
+print("Content written to example.txt successfully!")
